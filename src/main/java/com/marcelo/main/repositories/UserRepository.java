8f171be4bd0884ac86693487a850b04beef607a4
unknown <marcelocrisknauer@gmail.com>
2023-04-05 23:15:10 -0300

    Security configuration

--- src/main/java/com/marcelo/main/repositories/UserRepository.java
+++ src/main/java/com/marcelo/main/repositories/UserRepository.java
@@ -3,7 +3,9 @@ package com.marcelo.main.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
-public interface UserRepository extends JpaRepository<UserRepository, Long> {
+import com.marcelo.main.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 
 	UserDetails findByLogin(String login);
 
