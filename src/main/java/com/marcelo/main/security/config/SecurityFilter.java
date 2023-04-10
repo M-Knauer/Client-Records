3d8f6244840f7877d466cc657029cff0005af2d0
unknown <marcelocrisknauer@gmail.com>
2023-04-10 18:30:37 -0300

    Spring Security completo

--- src/main/java/com/marcelo/main/security/config/SecurityFilter.java
+++ src/main/java/com/marcelo/main/security/config/SecurityFilter.java
@@ -3,8 +3,12 @@ package com.marcelo.main.security.config;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.marcelo.main.repositories.UserRepository;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -16,6 +20,9 @@ public class SecurityFilter extends OncePerRequestFilter{
 	
 	@Autowired
 	private TokenService tokenService;
+	
+	@Autowired
+	private UserRepository repo;
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -23,8 +30,13 @@ public class SecurityFilter extends OncePerRequestFilter{
 		
 		var token = retriveToken(request);
 		
-		var subject = tokenService.getSubject(token);
-		System.out.println(subject);
+		if (token != null) {
+			var subject = tokenService.getSubject(token);
+			var user = repo.findByLogin(subject);
+			
+			var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+		}
 		
 		filterChain.doFilter(request, response);
 		
@@ -33,11 +45,11 @@ public class SecurityFilter extends OncePerRequestFilter{
 	private String retriveToken(HttpServletRequest request) {
 		var authHeader = request.getHeader("Authorization");
 		
-		if (authHeader == null) {
-			throw new RuntimeException("Token não enviado");
+		if (authHeader != null) {
+			return authHeader.replace("Bearer ", "");
 		}
 		
-		return authHeader.replace("Bearer ", "");
+		return null;
 	}
 
 }
